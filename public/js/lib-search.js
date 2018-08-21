56d72e596b8e2e692571642526912d31d6049299
Cloud Native Dev <ravichhabra@gmail.com>
2018-08-21 15:13:13 +0630

    improved all

--- public/js/lib-search.js
+++ public/js/lib-search.js
@@ -17,34 +17,30 @@ internsApp = {};
 	}
 
 	function fnRead(){
-		var path = 'interns/';
+		var key = $("#internID").val();
+		var path = 'interns/' + key;
 		fb.data.read(path, successFn, messageHandler);
 		function successFn(snapShot){
 			if(!snapShot){
 				console.log("No data found:");
 			}else{
-				var results = snapShot.val();
-				console.log(results);
-				var keys = Object.keys(results);
-				console.log("Key: " + keys); 
-				var interns = [];
-				keys.forEach(function(key){
+				console.log("Value :" + snapShot.val());
+					var result = snapShot.val();
 					var newElement = {}
-					var name = results[key].name;
-					var project = results[key].project;
-					var team = results[key].team;
+					var name = result.name;
+					var project = result.project;
+					var team = result.team;
 					newElement['key'] = key;
 					newElement['name'] = name;
 					newElement['team'] = team;
 					newElement['project'] = project;
+				  interns = [];
 					interns.push(newElement);
 					console.log(key, name, project, team);
-	  		});
-				console.log(interns);
+			}
 				internsObj = { internsBinding: interns}
 				ko.applyBindings(internsObj);
 		}
-}
 }
 
 	function messageHandler(err){
